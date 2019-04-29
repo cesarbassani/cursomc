6b51a690c40028ad0a65bede80645562d7d9b321
Cesar Bassani <cesarbassani@Cesars-MacBook-Pro.local>
2019-04-29 16:04:55 -0400

    Criando repository e service para Categoria

---
+++ src/main/java/com/cesarbassani/cursomc/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.cesarbassani.cursomc.services;
+
+import com.cesarbassani.cursomc.domain.Categoria;
+import com.cesarbassani.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public Categoria buscar(Integer id) {
+        Optional<Categoria> objCategoria = categoriaRepository.findById(id);
+        return objCategoria.orElse(null);
+    }
+}
